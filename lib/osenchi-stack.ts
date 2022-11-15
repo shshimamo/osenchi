e3c8867f5f88f4819de379bd68072738c5baa9b4
hiroya.shimamoto <shshimamo@gmail.com>
2022-11-15 10:05:37 +0900

    S3 と StepFunctions

--- lib/osenchi-stack.ts
+++ lib/osenchi-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class OsenchiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const inputBucket = new s3.Bucket(this, 'OsenchiInputBucket', {
+      bucketName: 'osenchi-inputbuchet'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OsenchiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const outputBucket = new s3.Bucket(this, 'OsenchiOutputBucket', {
+      bucketName: 'osenchi-outputbuchet'
+    });
+
+    const emailTopic = new sns.Topic(this, 'OsenchiEmailTopic', {
+      topicName: 'osenchi-topic'
+    });
+
+    const email = 'shshimamo@gmail.com';
+    emailTopic.addSubscription(new subscriptions.EmailSubscription(email));
+
+    const publishMessage = new tasks.SnsPublish(this, 'SendSuccessMail', {
+      topic: emailTopic,
+      subject: 'Osenchi Success',
+      message: sfn.TaskInput.fromJsonPathAt('$.*'),
+    });
+
+    new sfn.StateMachine(this, 'StateMachine', {
+      definition: publishMessage
+    });
   }
 }
