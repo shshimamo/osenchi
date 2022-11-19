4906da918ea840c4e3a1da191bc419ab1d7ac31f
hiroya.shimamoto <shshimamo@gmail.com>
2022-11-20 04:53:34 +0900

    assertions test, Validation test

--- test/osenchi.test.ts
+++ test/osenchi.test.ts
@@ -18,3 +18,85 @@ describe('Osenchi Snapshot tests', () => {
         expect(template).toMatchSnapshot();
     });
 });
+
+describe('Osenchi Fine-grained assertions tests', () => {
+    let app: cdk.App;
+    let stack: cdk.Stack;
+    let coreProps: ICoreProps;
+    let template: Template;
+    beforeEach(() => {
+        coreProps = {
+            bucket: {
+                inputName: 'test-input-bucket',
+                outputName: 'test-output-bucket',
+                logName: 'test-log-bucket',
+            },
+            topic: {
+                topicName: 'test-topic',
+                emails: ['test-email@example.com'],
+            },
+            detectionFuncName: 'test-detect-sentiment-function',
+            deletionFuncName: 'test-delete-object-function',
+        };
+        app = new cdk.App();
+        stack = new CoreStack(app, 'Osenchi-Core', coreProps);
+        template = Template.fromStack(stack);
+    });
+    // SNS
+    test('have a SNS Topic with EMail Subscriptions', () => {
+        template.hasResourceProperties('AWS::SNS::Topic', {
+            TopicName: coreProps.topic.topicName,
+        });
+    });
+    // S3
+    test('have input buckets', () => {
+        template.hasResourceProperties('AWS::S3::Bucket', {
+            BucketName: coreProps.bucket.inputName,
+        });
+    });
+    test('have output buckets', () => {
+        template.hasResourceProperties('AWS::S3::Bucket', {
+            BucketName: coreProps.bucket.outputName,
+        });
+    });
+    test('have log buckets', () => {
+        template.hasResourceProperties('AWS::S3::Bucket', {
+            BucketName: coreProps.bucket.logName,
+        });
+    });
+    // Lambda Function
+    test('have detect-sentiment function', () => {
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            FunctionName: coreProps.detectionFuncName,
+            Handler: 'index.handler',
+        });
+    });
+    test('have delete-object function', () => {
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            FunctionName: coreProps.deletionFuncName,
+            Handler: 'index.handler',
+        });
+    });
+});
+
+describe('Osenchi Validation tests', () => {
+    const coreProps: ICoreProps = {
+        bucket: {
+            inputName: 'test-input-bucket',
+            outputName: 'test-output-bucket',
+            logName: 'test-log-bucket',
+        },
+        topic: {
+            topicName: 'test-topic',
+            emails: [' test-email@example.com'],
+        },
+        detectionFuncName: 'test-detect-sentiment-function',
+        deletionFuncName: 'test-delete-object-function',
+    };
+    const app = new cdk.App();
+    test('Validation test of Email', () => {
+        expect(() => {
+            new CoreStack(app, 'Osenchi-Core', coreProps);
+        }).toThrowError('Email parameter is invalid.');
+    });
+});
